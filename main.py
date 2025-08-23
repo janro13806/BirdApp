7dff0509d11ae94557e16e9d0156f8d01b960f6a
janro13806 <80365036+janro13806@users.noreply.github.com>
2025-08-23 09:23:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -45,6 +45,6 @@ def predict():
     return jsonify({"predicted_class": label})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 80))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
  
